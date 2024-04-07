7ef2d0dceda522aa87ed0444317001ca516864e2
why <435371447@qq.com>
2024-04-08 00:04:08 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,19 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(weight:i32) -> i32 {
+    if weight > 40 {
+        weight
+    } else {
+        weight * 2
+    }
 
+
+
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
