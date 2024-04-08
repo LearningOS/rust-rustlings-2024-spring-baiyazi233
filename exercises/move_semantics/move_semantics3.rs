76d54ad0a7f30c9df7a3f10a0165473ae57b9dd3
why <435371447@qq.com>
2024-04-08 09:34:13 +0800

    update

--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -6,7 +6,7 @@
 // Execute `rustlings hint move_semantics3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let vec0 = Vec::new();
@@ -21,6 +21,7 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
     vec.push(22);
     vec.push(44);
     vec.push(66);
